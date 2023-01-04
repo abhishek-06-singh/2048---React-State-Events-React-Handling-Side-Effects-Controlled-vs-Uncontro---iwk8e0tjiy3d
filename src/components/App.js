23702b879db2e9a94087973c9d7f9f09de111b28
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2023-01-04 18:50:57 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import '../styles/App.css';
-import Board from './Board'
+import React from "react";
+import "../styles/App.css";
+import Board from "./Board";
 const App = () => {
-
   return (
     <div id="main">
       <h1>2048</h1>
       <Board GridSize={5} />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
